3e7009a7ce1908f19a0d778472492bb8d09d0225
Riya Agarwal <89921397+Riya00000@users.noreply.github.com>
2026-05-07 21:12:19 +0530

    out folder

--- kaplas-website/next.config.ts
+++ kaplas-website/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",                 // ← exports to plain HTML/CSS/JS
+  images: { unoptimized: true },    // ← needed because S3 has no image optimizer
+  trailingSlash: true,              // ← S3 prefers /about/ over /about
+
 };
 
 export default nextConfig;
